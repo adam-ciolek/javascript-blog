2b572a7d1b1f58150c3824cc58a2b9b86b551fcd
Adam Ciołek <adasciolek@gmail.com>
2021-04-28 18:47:48 +0200

    Add active link

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log(event);
 
   /* remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
+
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts .post');
