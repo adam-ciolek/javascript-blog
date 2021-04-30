939184f13ad32c9b3861aa2c89f323eda8dc7d43
Adam Ciołek <adasciolek@gmail.com>
2021-04-30 18:04:31 +0200

    Add js

--- js/script.js
+++ js/script.js
@@ -19,18 +19,21 @@ function titleClickHandler(event){
   clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.posts .post');
+  const activeArticles = document.querySelectorAll('.posts .post');
 
   for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-  const articleSelector = document.getElementById('#article-1');
-  articleSelector.clickedElement('href');
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active')
+
 }
 
 const links = document.querySelectorAll('.titles a');
