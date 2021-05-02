38d6987f834fae91f962a5872608f0702747c1cc
Adam Ciołek <adasciolek@gmail.com>
2021-05-02 14:13:32 +0200

    Remove .acitve from titles

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function titleClickHandler(event){
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+  activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
 
@@ -22,7 +22,7 @@ function titleClickHandler(event){
   const activeArticles = document.querySelectorAll('.posts .post');
 
   for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
+    activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
@@ -31,6 +31,7 @@ function titleClickHandler(event){
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
+  
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active')
 
