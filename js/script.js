6475f73d27d084d6ae52b095483eaff146f2ba41
Adam Ciołek <adasciolek@gmail.com>
2021-05-03 12:48:05 +0200

    Add js code

--- js/script.js
+++ js/script.js
@@ -42,3 +42,39 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = ''; 
+  function clearMessages(){
+    titleList.getElementById('messages').innerHTML = '';
+  }
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  
+   for(let article of articles){
+   
+    /* get the article id */
+  const articleId = article.getAttribute('id');
+  console.log(articleId);
+
+   
+    /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+  
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+  }
+}
+
+generateTitleLinks();
