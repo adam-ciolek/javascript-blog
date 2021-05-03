d9a8f9af5be6dc8b81275d0a917fcc1a086b61f2
Adam Ciołek <adasciolek@gmail.com>
2021-05-03 17:13:19 +0200

    Add js code

--- js/script.js
+++ js/script.js
@@ -46,18 +46,22 @@ function generateTitleLinks(){
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = ''; 
   /* for each article */
+
   const articles = document.querySelectorAll(optArticleSelector);
   for(let article of articles){
-    /* get the article id */
+  /* get the article id */
   const articleId = article.getAttribute('id'); 
-    console.log(articleId);
+  console.log(articleId);
 
-    /* find the title element */
-    /* get the title from the title element */
+  /* find the title element */
+  /* get the title from the title element */
   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    /* create HTML of the link */
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(linkHTML);
 
-    /* insert link into titleList */
+  /* insert link into titleList */
+  titleList.innerHTML = titleList.innerHTML + linkHTML;
   }
 }
 
