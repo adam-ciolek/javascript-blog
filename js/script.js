87842e89dbd7ee28943244ee55a2042af766c5c6
Adam Ciołek <adasciolek@gmail.com>
2021-05-05 17:54:58 +0200

    Add indents in lines in html and js

--- js/script.js
+++ js/script.js
@@ -49,23 +49,23 @@ function generateTitleLinks(){
   const articles = document.querySelectorAll(optArticleSelector);
   for(let article of articles){
 
-  /* get the article id */
-  const articleId = article.getAttribute('id'); 
-    console.log(articleId);
+    /* get the article id */
+    const articleId = article.getAttribute('id'); 
+      console.log(articleId);
   
-  /* find the title element */
-  /* get the title from the title element */
-  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-  /* create HTML of the link */ 
-  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    console.log(linkHTML);
+    /* create HTML of the link */ 
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
 
-  /* insert link into titleList */
+    /* insert link into titleList */
 
-  //titleList.innerHTML = titleList.innerHTML + linkHTML;
-  //titleList.insertAdjacentHTML('beforeend', linkHTML);
-  html = html + linkHTML;
+    //titleList.innerHTML = titleList.innerHTML + linkHTML;
+    //titleList.insertAdjacentHTML('beforeend', linkHTML);
+    html = html + linkHTML;
   }
 
   titleList.innerHTML = html;
